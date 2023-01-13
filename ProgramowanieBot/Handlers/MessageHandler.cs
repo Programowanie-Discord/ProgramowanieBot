12323b9d8bf488cdc741ef3e01ef9dddf98df734
KubaZ2 <jazloty@o2.pl>
2023-01-13 20:46:32 +0100

    Make adding reactions to post start messages instant

--- ProgramowanieBot/Handlers/MessageHandler.cs
+++ ProgramowanieBot/Handlers/MessageHandler.cs
@@ -41,46 +41,58 @@ internal partial class MessageHandler : BaseHandler<GuildThreadHandlerConfig>
         await task;
     }
 
-    private async ValueTask HandleMessageInHelpChannelAsync(Message message)
+    private ValueTask HandleMessageInHelpChannelAsync(Message message)
     {
         if (message.Channel is PublicGuildThread thread && thread.ParentId == Config.HelpChannelId)
         {
-            TaskCompletionSource taskCompletionSource = new();
-            Client.TypingStart += HandleTypingStartOnceAsync;
-            Client.MessageCreate += HandleMessageCreateOnceAsync;
-            await taskCompletionSource.Task.WaitAsync(_typingTimeout).ContinueWith(async task =>
+            if (message.Id == thread.Id)
+                return new(AddReactionsAsync());
+            else
             {
-                if (task.IsFaulted)
+                TaskCompletionSource taskCompletionSource = new();
+                Client.TypingStart += HandleTypingStartOnceAsync;
+                Client.MessageCreate += HandleMessageCreateOnceAsync;
+                return new(taskCompletionSource.Task.WaitAsync(_typingTimeout).ContinueWith(task =>
                 {
+                    if (task.IsFaulted)
+                    {
+                        Client.TypingStart -= HandleTypingStartOnceAsync;
+                        Client.MessageCreate -= HandleMessageCreateOnceAsync;
+                        return AddReactionsAsync();
+                    }
+                    return Task.CompletedTask;
+                }));
+
+                ValueTask HandleTypingStartOnceAsync(TypingStartEventArgs args)
+                {
+                    if (args.UserId != message.Author.Id || args.ChannelId != message.ChannelId)
+                        return default;
+
                     Client.TypingStart -= HandleTypingStartOnceAsync;
                     Client.MessageCreate -= HandleMessageCreateOnceAsync;
-                    await message.AddReactionAsync("⬆️");
-                    await message.AddReactionAsync("⬇️");
-                }
-            });
-
-            ValueTask HandleTypingStartOnceAsync(TypingStartEventArgs args)
-            {
-                if (args.UserId != message.Author.Id || args.ChannelId != message.ChannelId)
+                    taskCompletionSource.TrySetResult();
                     return default;
+                }
 
-                Client.TypingStart -= HandleTypingStartOnceAsync;
-                Client.MessageCreate -= HandleMessageCreateOnceAsync;
-                taskCompletionSource.TrySetResult();
-                return default;
+                ValueTask HandleMessageCreateOnceAsync(Message newMessage)
+                {
+                    if (newMessage.Author.Id != message.Author.Id || newMessage.ChannelId != message.ChannelId)
+                        return default;
+
+                    Client.TypingStart -= HandleTypingStartOnceAsync;
+                    Client.MessageCreate -= HandleMessageCreateOnceAsync;
+                    taskCompletionSource.TrySetResult();
+                    return default;
+                }
             }
 
-            ValueTask HandleMessageCreateOnceAsync(Message newMessage)
+            async Task AddReactionsAsync()
             {
-                if (newMessage.Author.Id != message.Author.Id || newMessage.ChannelId != message.ChannelId)
-                    return default;
-
-                Client.TypingStart -= HandleTypingStartOnceAsync;
-                Client.MessageCreate -= HandleMessageCreateOnceAsync;
-                taskCompletionSource.TrySetResult();
-                return default;
+                await message.AddReactionAsync("⬆️");
+                await message.AddReactionAsync("⬇️");
             }
         }
+        return default;
     }
 
     private async ValueTask HandleGitHubCodeAsync(Message message)
