4ebb5e8a172a9ad50321c54d22cb2575536b7a2a
KubaZ2 <jazloty@o2.pl>
2023-01-19 20:18:40 +0100

    Ignore duplicate emojis in `Steal Emojis`

--- ProgramowanieBot/Handlers/InteractionHandlerModules/Commands/MessageCommands/StealEmojisCommand.cs
+++ ProgramowanieBot/Handlers/InteractionHandlerModules/Commands/MessageCommands/StealEmojisCommand.cs
@@ -20,22 +20,32 @@ public partial class StealEmojisCommand : ApplicationCommandModule<ExtendedMessa
         {
             Components = new ComponentProperties[]
             {
-                new StringMenuProperties("steal-emoji", from match in matches
-                                                        let stringId = match.Groups[3].Value
-                                                        let result = (Success: ulong.TryParse(stringId, NumberStyles.None, CultureInfo.InvariantCulture, out var id), Id: id)
-                                                        where result.Success
-                                                        let animated = !match.Groups[1].ValueSpan.IsEmpty
-                                                        let name = match.Groups[2].Value
-                                                        select new StringMenuSelectOptionProperties(name, $"{animated}:{stringId}:{name}")
-                                                        {
-                                                            Emoji = new(result.Id),
-                                                        })
+                new StringMenuProperties("steal-emoji", GetOptions())
                 {
                     Placeholder = Context.Config.Interaction.StealEmoji.StealEmojisMenuPlaceholder,
                 },
             },
             Flags = MessageFlags.Ephemeral,
         }));
+
+        IEnumerable<StringMenuSelectOptionProperties> GetOptions()
+        {
+            HashSet<ulong> ids = new(25);
+            foreach (var match in matches.Take(25))
+            {
+                var stringId = match.Groups[3].Value;
+                if (!ulong.TryParse(stringId, NumberStyles.None, CultureInfo.InvariantCulture, out var id) || ids.Contains(id))
+                    continue;
+                ids.Add(id);
+
+                var animated = !match.Groups[1].ValueSpan.IsEmpty;
+                var name = match.Groups[2].Value;
+                yield return new(name, $"{animated}:{stringId}:{name}")
+                {
+                    Emoji = new(id),
+                };
+            }
+        }
     }
 
     [GeneratedRegex(@"<(a?):(\w+):(\d+)>")]
