7ddf3bcba477d1a4cb77c3dd7ccdbbb8b7670fb7
jedrek0429 <64475360+jedrek0429@users.noreply.github.com>
2024-02-13 17:03:32 +0100

    Change `postId` to `channelId`, as by convention this is ineed a channel ID and the word "post" should only be used when reffering to the database object

--- ProgramowanieBot/Helpers/PostsHelper.cs
+++ ProgramowanieBot/Helpers/PostsHelper.cs
@@ -8,15 +8,15 @@ namespace ProgramowanieBot.Helpers;
 
 internal static class PostsHelper
 {
-    public static async Task ResolvePostAsync(DataContext context, ulong postId)
+    public static async Task ResolvePostAsync(DataContext context, ulong channelId)
     {
         Debug.Assert(context.Database.CurrentTransaction != null, "Transaction is required.");
 
-        var post = await context.Posts.FirstOrDefaultAsync(p => p.PostId == postId);
+        var post = await context.Posts.FirstOrDefaultAsync(p => p.PostId == channelId);
         if (post == null)
             await context.Posts.AddAsync(new()
             {
-                PostId = postId,
+                PostId = channelId,
                 PostResolveReminderCounter = 0,
                 IsResolved = true,
             });
@@ -24,15 +24,15 @@ internal static class PostsHelper
             post.IsResolved = true;
     }
 
-    public static async Task IncrementPostResolveReminderCounterAsync(DataContext context, ulong postId)
+    public static async Task IncrementPostResolveReminderCounterAsync(DataContext context, ulong channelId)
     {
         Debug.Assert(context.Database.CurrentTransaction != null, "Transaction is required.");
 
-        var post = await context.Posts.FirstOrDefaultAsync(p => p.PostId == postId);
+        var post = await context.Posts.FirstOrDefaultAsync(p => p.PostId == channelId);
         if (post == null)
             await context.Posts.AddAsync(new()
             {
-                PostId = postId,
+                PostId = channelId,
                 PostResolveReminderCounter = 1,
                 IsResolved = false,
             });
